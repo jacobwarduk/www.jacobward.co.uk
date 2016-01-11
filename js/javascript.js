93484ab837eeea2931340044d7ad5f2ebb59b37e
Jacob Ward <jacobwarduk@users.noreply.github.com>
2016-01-11 19:11:23 +0000

    Ready for initial testing

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
-function parseXml(feed) {
+// IIFE to read latest data from WordPress RSS feed
+(function () {
   var parser = new DOMParser();
-  var xmlDoc = parser.parseFromString(feed, 'application/xml');
-  // alert(JSON.stringify(xmlDoc));
+  var xmlDoc = parser.parseFromString('http://www.jacobward.co.uk/feed', 'application/xml');
+  console.log(xmlDoc);
   return xmlDoc;
-}
+}());
