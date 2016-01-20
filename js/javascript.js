a5d913bed771786fa0b27954858e3b1b730c5c04
Jacob Ward <jacobwarduk@users.noreply.github.com>
2016-01-20 19:28:31 +0000

    Add header photo link and footer background

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,34 @@
 // IIFE to read latest data from WordPress RSS feed
+
+/*
 (function () {
-  var parser = new DOMParser();
-  var xmlDoc = parser.parseFromString('http://www.jacobward.co.uk/feed', 'application/xml');
-  console.log(xmlDoc);
-  return xmlDoc;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var parser = new DOMParser();
+      var xmlDoc = parser.parseFromString(xhttp.responseText, 'text/xml');
+      console.log(xmlDoc);
+      // return xmlDoc;
+      var posts = '';
+      title = xmlDoc.getElementsByTagName('title');
+      link = xmlDoc.getElementsByTagName('link');
+      date = xmlDoc.getElementsByTagName('pubDate');
+
+      posts += '<h2>Latest Posts</h2><ul>'
+
+      for (i = 1; i < title.length; i++) {
+        posts += '<article class="blog-post"><li><h3 class="post-title"><a href="' + link[i].childNodes[0].nodeValue + '">' + title[i].childNodes[0].nodeValue + '</a></h3></li></article>';
+      }
+
+      posts += '</ul><hr />'
+
+      document.getElementById('blog-section').innerHTML = posts;
+      console.log(posts);
+    }
+  };
+  xhttp.open('GET', 'http://www.jacobward.co.uk/feed/', true);
+  xhttp.send();
+
 }());
+*/
