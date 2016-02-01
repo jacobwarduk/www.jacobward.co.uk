38de4977610308d44d7890fc6e187a74bf0b9c3b
Jacob Ward <jacobwarduk@users.noreply.github.com>
2016-02-01 23:39:20 +0000

    Increase speed test performance grade

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,22 @@
 window.onload = function () {
     window.Modal = function ($internals) {
+
         var $close_btn, $modal, proto;
+
         if (typeof $internals === 'string') {
+
             proto = Modal.prototype;
             $modal = document.createElement('div');
+
             $modal.className = 'window-wrapper';
             $modal.innerHTML = '<section class="window">' + $internals + '<a href="#" class="close_btn">Close</a></section>';
+
             $close_btn = $modal.getElementsByClassName('close_btn')[0];
             $close_btn.addEventListener('click', function () {
                 document.body.removeChild($modal);
                 return proto.IsActive = false;
             });
+
             if (!proto.IsActive) {
                 document.body.appendChild($modal);
                 proto.IsActive = true;
@@ -21,16 +27,14 @@ window.onload = function () {
 
     window.Modal.prototype.IsActive = false;
     document.body.getElementsByClassName('pinterest-link')[0].addEventListener('click', function () {
-        return Modal('<iframe width="350" height="165" src="https://www.youtube.com/embed/qF7MsBtZx08?autoplay=true&t=0m8s" frameborder="0" allowfullscreen></iframe>', true);
+        return Modal('<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class="embed-container"><iframe src="http://www.youtube.com/embed/qF7MsBtZx08?autoplay=true" frameborder="0" allowfullscreen></iframe></div>', true);
     });
 };
 
 
 
 // IIFE to read latest data from WordPress RSS feed
-
-/*
-(function () {
+showPosts = function () {
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -42,22 +46,24 @@ window.onload = function () {
       var posts = '';
       title = xmlDoc.getElementsByTagName('title');
       link = xmlDoc.getElementsByTagName('link');
-      date = xmlDoc.getElementsByTagName('pubDate');
 
-      posts += '<h2>Latest Posts</h2><ul>'
+      posts += '<h2>Latest Posts</h2>'
 
       for (i = 1; i < title.length; i++) {
-        posts += '<article class="blog-post"><li><h3 class="post-title"><a href="' + link[i].childNodes[0].nodeValue + '">' + title[i].childNodes[0].nodeValue + '</a></h3></li></article>';
+        posts += '<article class="blog-post"><h3 class="post-title"><a href="' + link[i].childNodes[0].nodeValue + '">' + title[i].childNodes[0].nodeValue + '</a></h3></article>';
       }
 
-      posts += '</ul><hr />'
+      posts += '<hr />';
 
       document.getElementById('blog-section').innerHTML = posts;
+      window.location.hash = '#blog-section';
+      
       console.log(posts);
     }
   };
   xhttp.open('GET', 'http://www.jacobward.co.uk/feed/', true);
   xhttp.send();
 
-}());
-*/
+  return false;
+
+};
